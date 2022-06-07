1dca8bdf59b55087e6d64004658db24090883c32
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-07 04:58:44 -0700

    prints putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-	char word[8] = "_putchar";
-	int i;
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, size;
 
-	for (i = 0; i < 8; i++)
+	size = sizeof(str) / sizeof(int);
+	for (count = 0; count < size; count++)
 	{
-		_putchar(word[i]);
+		putchar(str[count]);
 	}
 	_putchar('\n');
-
 	return (0);
 }
