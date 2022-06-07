8137d5c510ca1b6d6725591aa5e94d33f815d275
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-07 00:23:03 -0700

    checks for lower case alphabets

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * main - checks for lower case alphabets
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for lowercase character. 0 for the rest
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
