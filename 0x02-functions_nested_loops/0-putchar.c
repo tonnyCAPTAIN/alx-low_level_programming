66a7e3962d1e251eed9d8cd81facf8d9546d7573
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-06 23:57:58 -0700

    Prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*
+ * main - Prints _putchar
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114}
+	int count, size;
+
+	size = sizeof(str) / sizeof(int);
+	for (count = 0; count < size; count++)
+	{
+		_putchar(str[count]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
