afb867191d6bf7661dbbafaf3bde2014c9c6387a
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-07 06:25:51 -0700

    function that checks for lowercase character.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - checks for lower case alphabets
+ * _islower- checks for lower case alphabets
  * @c: The character in ASCII code
  *
  * Return: 1 for lowercase character. 0 for the rest
