297c993b5bf1dc32f90ef2bce06971db2d879255
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-08 05:38:45 -0700

    create 1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,16 @@
+/**
+ * main - for infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
