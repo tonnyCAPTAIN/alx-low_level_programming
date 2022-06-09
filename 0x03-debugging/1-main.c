0dd0dd1b82eab6aeaa285698bdf8667de08e2832
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-09 02:19:56 -0700

    error corerction

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,7 +3,7 @@
 /**
  * * main - causes an infinite loop
  * * Return: 0
- * */
+ */
 
 int main(void)
 {
@@ -14,7 +14,8 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-/*while (i < 10)
+/*
+while (i < 10)
 {
 putchar(i);
 }
