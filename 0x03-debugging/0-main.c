65d1544cd1f36f594a5cf08f0babf4e6ca71a0cd
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-08 23:10:33 -0700

    positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * main - checks if integer is positive or negative
+ * main - checks if an integer is positive or negative
  * Return: 0
  */
 int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 
-	return (0);
+	Return (0);
 }
