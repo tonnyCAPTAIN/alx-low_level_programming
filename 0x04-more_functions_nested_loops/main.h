32a9b95f23652ea6df9be4573d068c4bb2335dc8
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-09 10:18:55 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#ifndef _main_h
+#define _main_h
+
+/*
+ * Function prototypes
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+
+#endif
