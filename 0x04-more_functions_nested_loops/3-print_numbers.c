289a90a6828bc928e08dd058495ddba214e8c98c
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-09 10:38:30 -0700

    print number 0 to 9 the newline

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints digits 0 - 9
+ *
+ */
+void print_number(void)
+{
+	int c;
+
+	for (c = 48; c <=57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
