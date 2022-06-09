ba57d733ce631cf77351c158a0e484e8a6506d7c
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-09 09:56:47 -0700

    upper alphabets

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - returns 1 if letter is uppercase
+ * 0 if it is not
+ *
+ * Return: returns 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
