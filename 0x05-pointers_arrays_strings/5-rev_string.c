50140c2f1ae6ebb42cec4c89305965e52aacd514
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-13 02:53:55 -0700

    prints in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - Prints a string in reverse
+ * @s: Input string
+ */
+void rev_string(char *s)
+{
+	int i, j, len;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
