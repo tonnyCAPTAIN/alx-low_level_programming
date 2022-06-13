a48d978b053544ec91a5c82f407c773ddd19f4c0
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-13 02:14:13 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen- finds the legth of a string
+ * @s: string
+ * Return: not.
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
+		
+
+	
