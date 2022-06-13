6eeded63d2f2cd81fa2607d29339ec7f1b45d912
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-13 02:37:52 -0700

    puts a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _puts - Prints a string
+ *
+ * @str: Input string
+ */
+void _puts(char *str)
+
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+
+
+
+
+		
