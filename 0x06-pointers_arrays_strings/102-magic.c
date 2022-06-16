521c52b78961dd8590dd0310490f1190b3bab95f
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-16 01:50:31 -0700

    main

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -12,7 +12,8 @@ int main(void)
 	 * write your line of code here...
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
-	 * - you are not allowed to code anything else than this line of code
+	 * - you are not allowed to code anything else than this line of 
+	 * 
 	 */
 	*(p + 5) = 98;
 	 /* ...so that this prints 98\n */
