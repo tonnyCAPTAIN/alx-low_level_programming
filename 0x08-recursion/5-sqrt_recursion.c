cd1f1b8c6a671b4a88a34d93f1ea8bdf80eba2ec
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-22 13:05:27 -0700

    squreroot of natural number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,7 +10,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
-		retrun (-1);
+		retrun(-1);
 	}
 	return (initial_sqrt_recursion(n, 0));
 }
@@ -19,14 +19,14 @@ int _sqrt_recursion(int n)
  * initial_sqrt_recursion - recurses to find the natural
  * number squreroot
  * @n: number to calculate the sqaure root of
- * @j
+ * @j:input
  * Return: the squareroot
  */
 
 int initial_sqrt_recursion(int n, int j)
 {
 	if (j * j > n)
-		return(-1);
+		return (-1);
 	if (j * j == n)
 		return (j);
 	return (initial_sqrt_recursion(n, j + 1));
