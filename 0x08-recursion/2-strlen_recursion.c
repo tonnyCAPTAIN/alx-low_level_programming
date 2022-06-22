210d88c0df406f7620ec3888d1ea2a41ae66276c
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-22 13:43:51 -0700

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,13 +2,13 @@
 /**
  * _strlen_recursion- returns length of string
  * @s- pointer to string
- * Return: Always 0
- **/
+ * Return: string length
+ */
+
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
-	{
+	if (*s == '\0')
+		return (0);
+	else
 		return (1 + _strlen_recursion(s + 1));
-	}
-	return (0);
 }
