f7c2ec83a76e0e2907ae536704e27b02fc0f3104
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-22 13:23:34 -0700

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * is_prime - return the sreqroot
+ * @n: INput
+ * @c: iterator
+ * Return: result
+ */
+
+int is_prime(unsigned int n, unsigned int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + is_prime(n, c + 1));
+}
+
+/**
+ * is_prime_number - find the natural prime number
+ * @n: input number
+ * Return: result
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (is_prime(n, 2));
+}
