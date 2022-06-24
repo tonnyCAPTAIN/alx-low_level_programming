9fd0e3e3e03065f090c2384f1fb84ef19a2331e8
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-24 03:05:43 -0700

    prints the array arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main- prints all arguments it receives
+ * @argc: count argument
+ * @argv: array argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int 1;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
