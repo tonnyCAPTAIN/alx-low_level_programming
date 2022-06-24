5b21b2d4d53f0b9de0b0f5feb359561d405e0b28
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-24 11:56:36 -0700

    static lib

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef _main_h
-#define _main_h
+#ifndef FILE_MAIN
+#define FILE_MAIN
 /*
  * function prototypes
  */
@@ -26,4 +26,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif/* MAIN_H */
