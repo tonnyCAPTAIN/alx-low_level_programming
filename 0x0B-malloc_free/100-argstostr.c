2793deb5bdd11f3c0fe735765fdba5118479cbd9
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-28 04:59:41 -0700

    function that concatenates all the arguments of your program.

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include"main.h"
+#include <stdlib.h>
+/**
+ * *argstostr - concatenates all arguements 
+ * @ac: arguement count
+ * @av: pointer to arguements
+ * Return: pointer to new space in memory or null
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *strDup;
+	int i, j, k, size;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	size = 0;
+	/* number of chars in each string*/
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			size++;
+		size++;
+	}size++;
+
+	/* allocate memory for total number of chars */
+
+	strDup = malloc(sizeof(char) * size);
+	if (strDup == NULL)
+		return (NULL);
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			strDup[k++] = av[i][j];
+		}
+		strDup[k++] = '\n';
+	}
+	strDup[k] = '\0';
+	return (strDup);
+}
