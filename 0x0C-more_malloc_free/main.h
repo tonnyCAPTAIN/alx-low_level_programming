9d0497b24183a2c73e2aefd559023650121c9de0
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-06-30 03:10:52 -0700

    function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef _main_h
+#define _main_h
+
+/*
+ * function prototypes
+ */
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif
