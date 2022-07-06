d759779c65ba86a6de20a61ee0b63fc33a938b9e
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-07-06 04:37:41 -0700

    function that prints a name.

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef _main_h
+#define _main_h
+
+/*
+ * Function prototypes
+ */
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,11 @@
+#ifndef _main_h
+#define _main_h
+
+/*
+ * Function prototype
+ */
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
