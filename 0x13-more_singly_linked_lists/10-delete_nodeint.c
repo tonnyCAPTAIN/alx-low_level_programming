2ce79b84a55b75a850f50b5cc55695d316d641b2
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-07-19 05:25:21 -0700

    freturns the sum of all the data (n) of a listint_t linked list.

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,39 +1,44 @@
-#include "list.h"
+#include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index 'index' of a linked list
- * @head: pointer to the first element in the list
- * @index: index of the node that should be deleted. Index starts at 0
- * Return: 1 if it succeeded, -1 if it failed
+ * delete_nodeint_at_index - deletes the node at index
+ * @head: head of a list.
+ * @index: index of the list where the node is deleted
+ * Return: 1 if it succeeded, -1 if it failed.
  */
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp = *head;
-	listint_t *current = NULL;
-	unsigned int i = 0;
+	unsigned int i;
+	listint_t *prev;
+	listint_t *next;
 
-	if (*head == NULL)
+	prev = *head;
+
+	if (index != 0)
+	{
+		for (i = 0; i < index - 1 && prev != NULL; i++)
+		{
+			prev = prev->next;
+		}
+	}
+	if (prev == NULL || (prev->next == NULL && index != 0))
+	{
 		return (-1);
-
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		free(temp);
-		return (1);
 	}
 
-	while (i < index - 1)
-	{
-		if (!temp || !(temp->next))
-			return (-1);
-		temp = temp->next;
-		i++;
-	}
+	next = prev->next;
 
-	current = temp->next;
-	temp->next = current->next;
-	free(current);
+	if (index != 0)
+	{
+		prev->next = next->next;
+		free(next);
+	}
+	else
+	{
+		free(prev);
+		*head = next;
+	}
 
 	return (1);
 }
--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 
 /**
  * sum_listint - sums all elements of a list
